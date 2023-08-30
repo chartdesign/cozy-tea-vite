ddb6d4254fd7d918b01b59a564dbf5fc92a6fabb
chartdesign <chartdesign@gamil.com>
2023-08-30 15:03:33 -0700

    firat commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+const Banner = () => {
+  return (
+    <div className="w-full bg-light-green text-center text-white py-1 text-xs sm:text-base">
+      <h3>
+        For free shipping on orders over $100.00 or more use code FREESHIPPINGYAY
+      </h3>
+    </div>
+  );
+};
+
+export default Banner;
